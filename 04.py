b600de25b8526f12db250cd6dfdac0beddc89246
Frederick Kim <freddy.kim8@gmail.com>
2020-02-04 15:24:16 -0800

    change feature 4 args

--- 04.py
+++ 04.py
@@ -1,4 +1,4 @@
 # **Input:** User id, year
 # **Output:** A dictionary with the different sources as keys and how much energy used per source in kWh
-def main(id: int, year: int) -> dict:
+def main(year: int) -> dict:
     return
