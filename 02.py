48e202ccdade6be4a1019d399efe033bb793b6b1
Frederick Kim <freddy.kim8@gmail.com>
2020-02-05 14:22:37 -0800

    change 02 month type

--- 02.py
+++ 02.py
@@ -1,5 +1,5 @@
 # **Input: U**ser id, year (YY), month (MM)
 # **Output:** Total amount in dollars the household should pay for that month
-def main(user_id: int, year: int, month: int) -> float:
+def main(user_id: int, year: int, month: str) -> float:
     # YOUR CODE HERE
     return
--- 05.py
+++ 05.py
@@ -1,7 +1,5 @@
-# **Input:** Nothing
-# **Output:** 
-#   1. The total environmental impact of the chosen combination of sources. 
-#   2. A dictionary of sources with the number of "blocks" per source with the environmental impact scale
+# **Input:** Environmental Impact Target
+# **Output:** An array of all the different combinations represented as dictionaries where the different sources are the keys and the total environmental impact of that source as the values
 def main(target: int) -> int, dict:
     # YOUR CODE HERE
     return
