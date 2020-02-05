d9071623755e4b1ee098758865dbeb9f4db17bea
Frederick Kim <freddy.kim8@gmail.com>
2020-02-04 16:42:34 -0800

    minor input change for feature 5

--- 05.py
+++ 05.py
@@ -2,6 +2,6 @@
 # **Output:** 
 #   1. The total environmental impact of the chosen combination of sources. 
 #   2. A dictionary of sources with the number of "blocks" per source with the environmental impact scale
-def main() -> int, dict:
+def main(target: int) -> int, dict:
     # YOUR CODE HERE
     return
